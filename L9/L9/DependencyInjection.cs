76645c044214d10a5f66a1e4a8999a1b9b24e720
horrorschau105 <piotrekgdowski@wp.pl>
2017-05-21 18:09:18 +0200

    Empty interface functions added

---
+++ L9/L9/DependencyInjection.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L9
+{
+    public class SimplyContainer
+    {
+        public SimplyContainer()
+        {
+            
+        }
+        public void RegisterType<T>(bool Singleton) where T : class
+        {
+
+        }
+        public void RegisterType<From, To>(bool Singleton) where To : From
+        {
+
+        }
+        public T Resolve<T>()
+        {
+            
+        }
+        
+    }
+}
