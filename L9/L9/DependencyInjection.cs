2c6ff76fde738a04f3052191557e45102c75d6f4
horrorschau105 <piotrekgdowski@wp.pl>
2017-05-21 18:32:19 +0200

    First attemps, first problems

--- L9/L9/DependencyInjection.cs
+++ L9/L9/DependencyInjection.cs
@@ -6,23 +6,32 @@ using System.Threading.Tasks;
 
 namespace L9
 {
-    public class SimplyContainer
+    public class SimplyContainer // should it be singleton?
     {
+        Dictionary<Type, object> singletons; // for constructed singletons
+        Dictionary<Type, bool> registeredTypes; //  foreach class remember if it wants singleton
+        Dictionary<Type, Type> registeredDependencies; // foreach class/interface remember deriving class
         public SimplyContainer()
         {
-            
+            registeredDependencies = new Dictionary<Type, Type>();
+            registeredTypes = new Dictionary<Type, bool>();
+            singletons = new Dictionary<Type, object>();
         }
         public void RegisterType<T>(bool Singleton) where T : class
         {
-
+            if (registeredTypes.ContainsKey(typeof(T))) registeredTypes[typeof(T)] = Singleton;
+            else registeredTypes.Add(typeof(T), Singleton); // simply add/change settings of class
         }
         public void RegisterType<From, To>(bool Singleton) where To : From
         {
-
+            if (registeredDependencies.ContainsKey(typeof(From))) registeredDependencies[typeof(From)] = typeof(To);
+            else registeredDependencies.Add(typeof(From), typeof(To)); // same as upper
+            // register 'To' type too
+           // RegisterType<To>(Singleton); // why can't I do it?
         }
-        public T Resolve<T>()
+        public T Resolve<T>() where T : class
         {
-            
+           // TODO
         }
         
     }
